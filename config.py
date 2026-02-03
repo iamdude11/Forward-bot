433069bc985586bcdfe7fc428ad89157a7ceb344
iamdude11 <yedhuku2003@gmail.com>
2026-02-03 08:44:44 +0530

    Update config.py
    New

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = int(os.environ.get("API_ID", "15529802"))
+API_HASH = os.environ.get("API_HASH", "92bcb6aa798a6f1feadbc917fccb54d3")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8348307399:AAH7Wnci84CVEECBtFurF6MZOxMrZJN4vwM")
 
-MONGO_URI = os.environ.get("MONGO_URI", "")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://RENDERTEST:RENDERTEST@cluster0.5mowi1z.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "SilentXBotz")
 
 WEB_SERVER = os.environ.get("WEB_SERVER", "True").lower() in ("true", "1", "t")
